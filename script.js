42d8947561d3395e15e6c6b9a83006e66c5cbe54
chtelo <chtello@escuelasproa.edu.ar>
2023-10-09 23:21:36 -0300

    inicios del dom

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-const background = document.querySelector('.background');
-background.addEventListener('click', () => {
-    background.style.animationPlayState = 'paused';
+let Br = document.getElementById("BarPl1");
+let Br2 = document.getElementById("BarPl2");
+
+let jugador1 = "";
+let jugador2 = "";
+
+const CPlayer1 = document.getElementById("GuarPlay1");
+CPlayer1.addEventListener("click", function ()  {
+    jugador1 = Br.value;
+    return jugador1
+});
+const DPlayer1 = document.getElementById("DeltPlay1");
+DPlayer1.addEventListener("click", function ()  {
+    Br.value = "";
+    jugador1 = "";
+    return jugador1
+});
+const CPlayer2 = document.getElementById("GuarPlay2");
+CPlayer2.addEventListener("click", function ()  {
+    jugador2 = Br2.value;
+    return jugador2
+});
+
+const DPlayer2 = document.getElementById("DeltPlay2");
+DPlayer2.addEventListener("click", function ()  {
+    Br2.value = "";
+    jugador2 = "";
+    return jugador2
 });
