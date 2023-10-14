ac29b1fca6643dfb079dfe8d8990128fa1a060d4
chtelo <chtello@escuelasproa.edu.ar>
2023-10-14 11:59:03 -0300

    Ya falta menos

---
+++ src/SrcJuego.js
@@ -0,0 +1,97 @@
+let turno = 2;
+
+const Tec1 = document.getElementById("tecla1");
+Tec1.addEventListener("click", function ()  {
+    if (Tec1.textContent == "" && turno %2 == 1) {
+        Tec1.textContent = "x"
+    }
+    else if (Tec1.textContent == "" && turno % 2 == 0) {
+        Tec1.textContent = "O"
+    }
+    turno ++;
+});
+const Tec2 = document.getElementById("tecla2");
+Tec2.addEventListener("click", function ()  {
+    if (Tec2.textContent == "" && turno %2 == 1) {
+        Tec2.textContent = "x"
+    }
+    else if (Tec2.textContent == "" && turno % 2 == 0) {
+        Tec2.textContent = "O"
+    }
+    turno ++;
+});
+const Tec3 = document.getElementById("tecla3");
+Tec3.addEventListener("click", function ()  {
+    if (Tec3.textContent == "" && turno %2 == 1) {
+        Tec3.textContent = "x"
+    }
+    else if (Tec3.textContent == "" && turno % 2 == 0) {
+        Tec3.textContent = "O"
+    }
+    turno ++;
+});
+const Tec4 = document.getElementById("tecla4");
+Tec4.addEventListener("click", function ()  {
+    if (Tec4.textContent == "" && turno %2 == 1) {
+        Tec4.textContent = "x"
+    }
+    else if (Tec4.textContent == "" && turno % 2 == 0) {
+        Tec4.textContent = "O"
+    }
+    turno ++;
+});
+const Tec5 = document.getElementById("tecla5");
+Tec5.addEventListener("click", function ()  {
+    if (Tec5.textContent == "" && turno %2 == 1) {
+        Tec5.textContent = "x"
+    }
+    else if (Tec5.textContent == "" && turno % 2 == 0) {
+        Tec5.textContent = "O"
+    }
+    turno ++;
+});
+const Tec6 = document.getElementById("tecla6");
+Tec6.addEventListener("click", function ()  {
+    if (Tec6.textContent == "" && turno %2 == 1) {
+        Tec6.textContent = "x"
+    }
+    else if (Tec6.textContent == "" && turno % 2 == 0) {
+        Tec6.textContent = "O"
+    }
+    turno ++;
+});
+
+const Tec7 = document.getElementById("tecla7");
+Tec7.addEventListener("click", function ()  {
+    if (Tec7.textContent == "" && turno %2 == 1) {
+        Tec7.textContent = "x"
+    }
+    else if (Tec7.textContent == "" && turno % 2 == 0) {
+        Tec7.textContent = "O"
+    }
+    turno ++;
+});
+const Tec8 = document.getElementById("tecla8");
+Tec8.addEventListener("click", function ()  {
+    if (Tec8.textContent == "" && turno %2 == 1) {
+        Tec8.textContent = "x"
+    }
+    else if (Tec8.textContent == "" && turno % 2 == 0) {
+        Tec8.textContent = "O"
+    }
+    turno ++;
+});const Tec9 = document.getElementById("tecla9");
+Tec9.addEventListener("click", function ()  {
+    if (Tec9.textContent == "" && turno %2 == 1) {
+        Tec9.textContent = "x"
+    }
+    else if (Tec9.textContent == "" && turno % 2 == 0) {
+        Tec9.textContent = "O"
+    }
+    turno ++;
+});
+
+
+
+
+
